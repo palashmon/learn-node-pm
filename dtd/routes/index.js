84e90cdd9da78f96162f2cd682335d8ff791ca62
Palash Mondal <palashm@live.in>
2017-06-11 11:28:34 +0530

    Updated routing code base

--- dtd/routes/index.js
+++ dtd/routes/index.js
@@ -1,9 +1,28 @@
 const express = require('express');
+
 const router = express.Router();
 
 // Do work here
 router.get('/', (req, res) => {
-  res.send('Hey! It works!');
+	const obj = { name: 'Palash', age: 100, cool: true};
+  //res.send('Hey! It works 🎉🎉🎉');
+
+	// Send json data back as response
+	//res.json(obj);
+
+	// Get request query params here using `req`
+	// Check if `req.query` is an empty object
+	if(Object.keys(req.query).length === 0)
+		res.send('No request query string found...');		
+	else
+		res.send(req.query);
+
+});
+
+// New route to reverse
+router.get('/reverse/:name', (req, res) => {
+	const reverse = [...req.params.name].reverse().join('');
+	res.send(`Request param name value is: ${req.params.name}<br/>And reverse value is: ${reverse}`);
 });
 
 module.exports = router;
--- dtd/start.js
+++ dtd/start.js
@@ -22,6 +22,7 @@ mongoose.connection.on('error', (err) => {
 
 // Start our app!
 const app = require('./app');
+
 app.set('port', process.env.PORT || 7777);
 const server = app.listen(app.get('port'), () => {
   console.log(`Express running → PORT ${server.address().port}`);
