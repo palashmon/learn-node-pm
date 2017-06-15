fc0420b748797810281a5a17010c0f336af06cbb
Palash Mondal <palashm@live.in>
2017-06-15 20:07:25 +0530

    Updated template helpers, mvc pattern code base

--- dtd/routes/index.js
+++ dtd/routes/index.js
@@ -1,32 +1,7 @@
 const express = require('express');
-
+const storeController = require('../controllers/storeController');
 const router = express.Router();
 
-// Do work here
-router.get('/', (req, res) => {
-	const obj = { name: 'Palash', age: 100, cool: true};
-  //res.send('Hey! It works 🎉🎉🎉');
-
-	// Send json data back as response
-	//res.json(obj);
-
-	// Get request query params here using `req`
-	// Check if `req.query` is an empty object
-	// if(Object.keys(req.query).length === 0)
-	// 	res.send('No request query string found...');		
-	// else
-	// 	res.send(req.query);
-
-  // Using the hello pug file inside the view folder
-	res.render('hello', {
-		name: 'Palash', age: 100
-	});
-});
-
-// New route to reverse
-router.get('/reverse/:name', (req, res) => {
-	const reverse = [...req.params.name].reverse().join('');
-	res.send(`Request param name value is: ${req.params.name}<br/>And reverse value is: ${reverse}`);
-});
+router.get('/', storeController.homePage);
 
 module.exports = router;
