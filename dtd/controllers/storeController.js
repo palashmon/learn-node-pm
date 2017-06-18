25a712bc0ec3ee4c8e3e78511a1c3c58e7f6770e
Palash Mondal <palashm@live.in>
2017-06-18 13:01:11 +0530

    Updated middleware and error handler code base

--- dtd/controllers/storeController.js
+++ dtd/controllers/storeController.js
@@ -1,3 +1,12 @@
+exports.myMiddleware = (req, res, next) => {
+	req.name = 'Palash';
+	if(req.name === 'Palash'){
+		throw Error('Please enter a valid name!');
+	}
+	next();
+};
+
 exports.homePage = (req, res) => {
+	console.log(req.name);
   res.render('index', { title: 'Home Page' });
 };
--- dtd/routes/index.js
+++ dtd/routes/index.js
@@ -2,6 +2,6 @@ const express = require('express');
 const storeController = require('../controllers/storeController');
 const router = express.Router();
 
-router.get('/', storeController.homePage);
+router.get('/', storeController.myMiddleware, storeController.homePage);
 
 module.exports = router;
