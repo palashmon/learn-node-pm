c1c685774b1ffd9e7857435efe33e8f47eb1212d
Palash Mondal <palashm@live.in>
2017-06-23 21:39:05 +0530

    Updated saving stores & mixins code base

--- dtd/controllers/storeController.js
+++ dtd/controllers/storeController.js
@@ -1,3 +1,13 @@
 exports.homePage = (req, res) => {
   res.render('index', { title: 'Home Page' });
 };
+
+// GET
+exports.addStore = (req, res) => {
+  res.render('editStore', { title: 'Add Store' });
+};
+
+// POST
+exports.createStore = (req, res) => {
+  res.json(req.body);
+};
--- dtd/routes/index.js
+++ dtd/routes/index.js
@@ -4,4 +4,8 @@ const router = express.Router();
 
 router.get('/', storeController.homePage);
 
+// Add routes for adding a new store
+router.get('/add', storeController.addStore);
+router.post('/add', storeController.createStore);
+
 module.exports = router;
