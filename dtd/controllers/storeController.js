86b21f99e4f9e44153e3505eff27cfe82527265c
Palash Mondal <palashm@live.in>
2017-06-27 19:41:02 +0530

    Updated flash messages code base

--- dtd/controllers/storeController.js
+++ dtd/controllers/storeController.js
@@ -12,8 +12,8 @@ exports.addStore = (req, res) => {
 
 // POST
 exports.createStore = async (req, res) => {
-  const store = new Store(req.body);
-  await store.save();
-  res.redirect('/');
+  const store = await (new Store(req.body)).save();
+  req.flash('success', `Successfully Created ${store.name}. Care to leave a review?`);
+  res.redirect(`/store/${store.slug}`);
 };
 
