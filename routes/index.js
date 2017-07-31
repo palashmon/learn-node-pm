98e679afd3670e08d526bf5185bafb43650ecd67
Palash Mondal <palashm@live.in>
2017-07-31 19:35:12 +0530

    Added logic for password reset flow

--- routes/index.js
+++ routes/index.js
@@ -59,4 +59,12 @@ router.get('/logout', authController.logout);
 router.get('/account', authController.isLoggedIn, userController.account);
 router.post('/account', catchErrors(userController.updateAccount));
 
+router.post('/account/forgot', catchErrors(authController.forgot));
+router.get('/account/reset/:token', catchErrors(authController.reset));
+router.post(
+  '/account/reset/:token',
+  authController.confirmedPasswords,
+  catchErrors(authController.update)
+);
+
 module.exports = router;
