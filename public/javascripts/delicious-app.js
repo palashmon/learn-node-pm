9aae5679e77d95e0e462cc09a9aea795501d896a
Palash Mondal <palashm@live.in>
2017-08-05 13:29:35 +0530

    Added modified main delicious-app js code

--- public/javascripts/delicious-app.js
+++ public/javascripts/delicious-app.js
@@ -3,5 +3,8 @@ import '../sass/style.scss';
 /* eslint no-unused-vars: 0 */
 import { $, $$ } from './modules/bling';
 import autocomplete from './modules/autocomplete';
+import typeAhead from './modules/typeAhead';
 
 autocomplete($('#address'), $('#lat'), $('#lng'));
+
+typeAhead($('.search'));
