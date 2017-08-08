b1619c35ab78962ccc1e252ccb9a62b613694fd2
Palash Mondal <palashm@live.in>
2017-08-08 20:44:36 +0530

    Added makeMap to main module

--- public/javascripts/delicious-app.js
+++ public/javascripts/delicious-app.js
@@ -4,7 +4,9 @@ import '../sass/style.scss';
 import { $, $$ } from './modules/bling';
 import autocomplete from './modules/autocomplete';
 import typeAhead from './modules/typeAhead';
+import makeMap from './modules/map';
 
 autocomplete($('#address'), $('#lat'), $('#lng'));
 
 typeAhead($('.search'));
+makeMap($('#map'));
