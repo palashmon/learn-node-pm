1ec85b9ad175223b4e4d069911674d73c5708eaf
Palash Mondal <palashm@live.in>
2017-08-09 21:11:50 +0530

    Updated dist files for hearts logic addition

--- public/dist/App.bundle.js
+++ public/dist/App.bundle.js
@@ -973,13 +973,22 @@ var _map = __webpack_require__(32);
 
 var _map2 = _interopRequireDefault(_map);
 
+var _heart = __webpack_require__(39);
+
+var _heart2 = _interopRequireDefault(_heart);
+
 function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
 
-/* eslint no-unused-vars: 0 */
 (0, _autocomplete2.default)((0, _bling.$)('#address'), (0, _bling.$)('#lat'), (0, _bling.$)('#lng'));
+
+/* eslint no-unused-vars: 0 */
+
 
 (0, _typeAhead2.default)((0, _bling.$)('.search'));
 (0, _map2.default)((0, _bling.$)('#map'));
+
+var heartForms = (0, _bling.$$)('form.heart');
+heartForms.on('submit', _heart2.default);
 
 /***/ }),
 /* 11 */
@@ -2801,6 +2810,51 @@ function makeMap(mapDiv) {
 }
 
 exports.default = makeMap;
+
+/***/ }),
+/* 33 */,
+/* 34 */,
+/* 35 */,
+/* 36 */,
+/* 37 */,
+/* 38 */,
+/* 39 */
+/***/ (function(module, exports, __webpack_require__) {
+
+"use strict";
+
+
+Object.defineProperty(exports, "__esModule", {
+  value: true
+});
+
+var _axios = __webpack_require__(3);
+
+var _axios2 = _interopRequireDefault(_axios);
+
+var _bling = __webpack_require__(2);
+
+function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
+
+function ajaxHeart(e) {
+  var _this = this;
+
+  e.preventDefault();
+  // console.log('HEART ITTT!!!!!!!!!!!!!!!!');
+  // console.log(this);
+  _axios2.default.post(this.action).then(function (res) {
+    var isHearted = _this.heart.classList.toggle('heart__button--hearted');
+    (0, _bling.$)('.heart-count').textContent = res.data.hearts.length;
+    if (isHearted) {
+      _this.heart.classList.add('heart__button--float');
+      setTimeout(function () {
+        return _this.heart.classList.remove('heart__button--float');
+      }, 2500);
+    }
+  }).catch(console.error);
+}
+
+exports.default = ajaxHeart;
 
 /***/ })
 /******/ ]);
