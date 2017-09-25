ddcd96d1e04a2345cf3224d3907f4b42bf98b8a0
Palash Mondal <palashm@live.in>
2017-09-25 12:52:00 +0530

    Update babel preset to env I'm getting the deprecation warning for babel-present-es2015 so let's update it to the latest More info: https://github.com/babel/babel-preset-env

--- webpack.config.js
+++ webpack.config.js
@@ -15,13 +15,13 @@ const autoprefixer = require('autoprefixer');
 
 // This is our JavaScript rule that specifies what to do with .js files
 const javascript = {
-  test: /\.(js)$/, // see how we match anything that ends in `.js`? Cool
-  use: [
-    {
-      loader: 'babel-loader',
-      options: { presets: ['es2015'] } // this is one way of passing options
-    }
-  ]
+    test: /\.(js)$/, // see how we match anything that ends in `.js`? Cool
+    use: [
+        {
+            loader: 'babel-loader',
+            options: { presets: ['env'] } // this is one way of passing options
+        }
+    ]
 };
 
 /*
@@ -29,76 +29,76 @@ const javascript = {
 */
 
 const postcss = {
-  loader: 'postcss-loader',
-  options: {
-    plugins() {
-      return [autoprefixer({ browsers: 'last 3 versions' })];
+    loader: 'postcss-loader',
+    options: {
+        plugins() {
+            return [autoprefixer({ browsers: 'last 3 versions' })];
+        }
     }
-  }
 };
 
 // this is our sass/css loader. It handles files that are require('something.scss')
 const styles = {
-  test: /\.(scss)$/,
-  // here we pass the options as query params b/c it's short.
-  // remember above we used an object for each loader instead of just a string?
-  // We don't just pass an array of loaders, we run them through the extract plugin so they can be outputted to their own .css file
-  use: ExtractTextPlugin.extract(['css-loader?sourceMap', postcss, 'sass-loader?sourceMap'])
+    test: /\.(scss)$/,
+    // here we pass the options as query params b/c it's short.
+    // remember above we used an object for each loader instead of just a string?
+    // We don't just pass an array of loaders, we run them through the extract plugin so they can be outputted to their own .css file
+    use: ExtractTextPlugin.extract(['css-loader?sourceMap', postcss, 'sass-loader?sourceMap'])
 };
 
 // We can also use plugins - this one will compress the crap out of our JS
 const uglify = new webpack.optimize.UglifyJsPlugin({
-  // eslint-disable-line
-  compress: { warnings: false }
+    // eslint-disable-line
+    compress: { warnings: false }
 });
 
 // OK - now it's time to put it all together
 const config = {
-  entry: {
-    // we only have 1 entry, but I've set it up for multiple in the future
-    App: './public/javascripts/delicious-app.js'
-  },
-  // we're using sourcemaps and here is where we specify which kind of sourcemap to use
-  devtool: 'source-map',
-  // Once things are done, we kick it out to a file.
-  output: {
-    // path is a built in node module
-    // __dirname is a variable from node that gives us the
-    path: path.resolve(__dirname, 'public', 'dist'),
-    // we can use "substitutions" in file names like [name] and [hash]
-    // name will be `App` because that is what we used above in our entry
-    filename: '[name].bundle.js'
-  },
+    entry: {
+        // we only have 1 entry, but I've set it up for multiple in the future
+        App: './public/javascripts/delicious-app.js'
+    },
+    // we're using sourcemaps and here is where we specify which kind of sourcemap to use
+    devtool: 'source-map',
+    // Once things are done, we kick it out to a file.
+    output: {
+        // path is a built in node module
+        // __dirname is a variable from node that gives us the
+        path: path.resolve(__dirname, 'public', 'dist'),
+        // we can use "substitutions" in file names like [name] and [hash]
+        // name will be `App` because that is what we used above in our entry
+        filename: '[name].bundle.js'
+    },
 
-  // remember we said webpack sees everthing as modules and how different loaders are responsible for different file types? Here is is where we implement them. Pass it the rules for our JS and our styles
-  module: {
-    rules: [javascript, styles]
-  },
-  // finally we pass it an array of our plugins - uncomment if you want to uglify
-  // plugins: [uglify]
-  plugins: [
-    // here is where we tell it to output our css to a separate file
-    new ExtractTextPlugin('style.css'),
+    // remember we said webpack sees everything as modules and how different loaders are responsible for different file types? Here is is where we implement them. Pass it the rules for our JS and our styles
+    module: {
+        rules: [javascript, styles]
+    },
+    // finally we pass it an array of our plugins - uncomment if you want to uglify
+    // plugins: [uglify]
+    plugins: [
+        // here is where we tell it to output our css to a separate file
+        new ExtractTextPlugin('style.css'),
 
-    // new BundleAnalyzerPlugin({
-    //   analyzerMode: 'static'
-    // }),
+        // new BundleAnalyzerPlugin({
+        //   analyzerMode: 'static'
+        // }),
 
-    new webpack.optimize.UglifyJsPlugin({
-      // eslint-disable-line
-      compress: { warnings: false }
-    }),
+        new webpack.optimize.UglifyJsPlugin({
+            // eslint-disable-line
+            compress: { warnings: false }
+        }),
 
-    new webpack.optimize.CommonsChunkPlugin({
-      name: 'vendor',
-      minChunks: m => m.context && m.context.includes('node_modules')
-    }),
-    new webpack.optimize.CommonsChunkPlugin({
-      name: 'runtime',
-      chunks: ['vendor'],
-      minChunks: Infinity
-    })
-  ]
+        new webpack.optimize.CommonsChunkPlugin({
+            name: 'vendor',
+            minChunks: m => m.context && m.context.includes('node_modules')
+        }),
+        new webpack.optimize.CommonsChunkPlugin({
+            name: 'runtime',
+            chunks: ['vendor'],
+            minChunks: Infinity
+        })
+    ]
 };
 // webpack is cranky about some packages using a soon to be deprecated API. shhhhhhh
 process.noDeprecation = true;
